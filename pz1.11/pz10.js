cf9dc3a5fdcee419c837d14c82dfc5438a607513
John <johnDou@mail.com>
2024-05-03 11:31:58 +0300

    commit

---
+++ pz1.11/pz10.js
@@ -0,0 +1,14 @@
+function reverseAndSquare(arr) {
+    // Перевертаємо масив
+    const reversedArr = arr.reverse();
+    
+    // Підносимо до квадрату числові значення
+    const squaredArr = reversedArr.map(item => typeof item === 'number' ? item ** 2 : item);
+    
+    return squaredArr;
+}
+
+// Приклад виклику функції
+const inputArray = [1, 2, 3, 'a', 'b', 'c'];
+const result = reverseAndSquare(inputArray);
+console.log(result); // Результат: [9, 4, 1, "b", "a", "c"]
